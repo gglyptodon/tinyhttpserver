f5764cf6a3867b89a95740711f5fc11f61ae8245
Janina Mass <janina.mass@gmail.com>
2022-06-27 15:38:21 +0200

    refactor

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,6 @@ fn main() {
     println!("Running on {}\n", address);
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        println!("connection request from: {}", &stream.peer_addr().unwrap());
         handle_connection(stream);
     }
 }
@@ -21,27 +20,18 @@ fn handle_connection(mut stream: TcpStream) {
                 template_file_as_string)};
     let mut buffer = [0u8; 1024];
     stream.read(&mut buffer).unwrap();
-    println!("{:}", format!("{}", "*".repeat(30)));
-    println!("Request: {} ", String::from_utf8_lossy(&buffer));
-    println!("{:}", format!("{}", "*".repeat(30)));
     let get_root = b"GET / HTTP/1.1\r\n";
-
     let html_ok = "HTTP/1.1 200 OK\r\n";
-    if buffer.starts_with(get_root) {
-        let contents = fs::read_to_string("index.htm").expect("Failed to read html template");
-        let response = format!(
-            "{}Content-Length:{}\r\n\r\n{}",
-            html_ok,
-            contents.len(),
-            contents
-        );
-        stream.write(response.as_bytes()).unwrap();
-        stream.flush().unwrap();
+    let html_file_not_found = "HTTP/1.1 404 NOT FOUND\r\n";
+
+    let (status_line, filename) = if buffer.starts_with(get_root) {
+        (html_ok, "index.htm")
     } else {
-        let html_file_not_found = "HTTP/1.1 404 NOT FOUND\r\n";
-        let html_file = fs::read_to_string("404.htm").expect("Failed to read 404 template");
-        let response = template(html_file_not_found, html_file);
-        stream.write(response.as_bytes()).unwrap();
-        stream.flush().unwrap();
-    }
+        (html_file_not_found, "404.htm")
+    };
+    let html_file = fs::read_to_string(filename).expect(&*format!("Failed to read template {}", filename));
+    let response = template(status_line, html_file);
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+
 }
