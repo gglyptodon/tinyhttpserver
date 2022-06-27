15ae8fcd91719fb528a767132b1dc3e65cfdc34f
Janina Mass <janina.mass@gmail.com>
2022-06-27 15:05:41 +0200

    return html response

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::fs;
 use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
 
@@ -21,8 +22,11 @@ fn handle_connection(mut stream: TcpStream){
     println!("Request: {} ", String::from_utf8_lossy(&buffer));
     println!("{:}", format!("{}","*".repeat(30)));
 
-    //let response = "HTTP/1.1 200 OK \r\n\r\n";
-    let response = "HTTP/1.1 418 I'm a teapot\r\n\r\n";
+
+    let html_ok = "HTTP/1.1 200 OK\r\n";
+    //let response = "HTTP/1.1 418 I'm a teapot\r\n\r\n";
+    let contents = fs::read_to_string("index.htm").expect("Failed to read html template");
+    let response = format!("{}Content-Length:{}\r\n\r\n{}", html_ok, contents.len(), contents);
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
 
