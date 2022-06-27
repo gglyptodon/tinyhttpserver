fa157c7bb20be7da52332b6586d2611d92a5084f
Janina Mass <janina.mass@gmail.com>
2022-06-27 14:55:18 +0200

    basic http response

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,29 @@
-use std::net::TcpListener;
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
+
 fn main() {
-    let address = "127.0.0.1:8887";
+    let address = "::1:8887";
+    //let address = "127.0.0.1:8887";
     let listener = TcpListener::bind(address).expect(&*format!("cannot bind to {}", address));
-    println!("Running on {}", address);
+    println!("Running on {}\n", address);
     for stream in listener.incoming(){
         let stream = stream.unwrap();
-        println!("connection established")
+        println!("connection request from: {}",&stream.peer_addr().unwrap());
+        handle_connection(stream);
     }
+
+}
+
+fn handle_connection(mut stream: TcpStream){
+    let mut buffer = [0u8;1024];
+    stream.read(&mut buffer).unwrap();
+    println!("{:}", format!("{}","*".repeat(30)));
+    println!("Request: {} ", String::from_utf8_lossy(&buffer));
+    println!("{:}", format!("{}","*".repeat(30)));
+
+    //let response = "HTTP/1.1 200 OK \r\n\r\n";
+    let response = "HTTP/1.1 418 I'm a teapot\r\n\r\n";
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 
 }
